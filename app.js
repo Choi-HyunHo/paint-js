1c52bce5d520ae4d999f08f397741893e796213f
최현호 <x7788@naver.com>
2021-11-23 16:48:11 +0900

    ctx 설정 중

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "#2c2c2c"; // default 색상 
+ctx.lineWidth = 2.5; // 선의 굵기
 
 let painting = false;
 
@@ -6,9 +10,20 @@ function stopPainting(){
     painting = false;
 }
 
+function startPainting(){
+    painting = true;
+}
+
 function onMouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
+    if(!painting){  // 클릭하고 움직이면 실행되지 않는다.
+        ctx.beginPath(); // 경로 생성
+        ctx.moveTo(x,y); // 선 시작 좌표
+    } else {
+        ctx.lineTo(x,y); // 선 끝 좌표
+        ctx.stroke(); // 선 그리기
+    }
 }
 
 function onMouseDown(event){
@@ -22,7 +37,7 @@ function onMouseUp(event){
 
 if(canvas){
     canvas.addEventListener("mousemove",onMouseMove); // 캔버스 위에서 움직일 때
-    canvas.addEventListener("mousedown", onMouseDown); // 캔버스 위에서 클릭 했을 때
-    canvas.addEventListener("mouseup", onMouseUp); // 캔버스 위에서 클릭 해제
+    canvas.addEventListener("mousedown", startPainting); // 캔버스 위에서 클릭 했을 때
+    canvas.addEventListener("mouseup", stopPainting); // 캔버스 위에서 클릭 해제
     canvas.addEventListener("mouseleave", stopPainting); // 마우스가 캔버스 위에 벗어날 경우
 }
